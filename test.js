06de0455d7b3a7e2958e600ac61f15883aa3cd20
Sean Gurson <sdgurson@gmail.com>
2018-08-25 16:32:46 -0700

    First commit

---
+++ test.js
@@ -0,0 +1,35 @@
+var finance = require('./financeModel.js'); //without ./ it looks in node modules
+
+(async function() {
+	console.log('****sean');
+
+	try {
+
+		const result = await finance.getBRKB(); // await has to be used inside an async function
+		console.log('result:', result.data.optionChain.result[0].underlyingSymbol);
+
+		const price2 = await finance.getData(); //this lines is equivalent to 
+		console.log(price2.data.optionChain.result[0].underlyingSymbol);
+	}
+	catch (err) {
+		console.log(err);
+	}
+})();
+
+
+console.log("++++Sean");
+
+finance.getBRKB().then((result) => { 
+	console.log(result.data.optionChain.result[0].underlyingSymbol);
+	return finance.getData(result.xzy);
+})
+.then(price2 => {
+	console.log(price2.data.optionChain.result[0].underlyingSymbol); //this line
+})
+.catch(err => {
+
+}) 
+
+
+
+
